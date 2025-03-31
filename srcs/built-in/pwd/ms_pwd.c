f8a82d7b5a272593e9abe9c3dcf1333be475a057
Arthur Girard <artgirar@c1r7p9.42lehavre.fr>
2025-03-31 15:48:35 +0200

    pwd finished

--- srcs/built-in/pwd/ms_pwd.c
+++ srcs/built-in/pwd/ms_pwd.c
@@ -6,8 +6,17 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:44:02 by maillet           #+#    #+#             */
-/*   Updated: 2025/03/25 15:09:25 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/03/31 15:48:06 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.function.h"
+
+void	pwd(void)
+{
+	char	*path;
+
+	path = getcwd(NULL, 0);
+	printf("%s\n", path);
+	free(path);
+}
