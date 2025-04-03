c86c486332c70067095647067a89b669d5c03163
Arthur Girard <artgirar@c1r7p12.42lehavre.fr>
2025-04-03 11:21:27 +0200

    modification : restart ms_exec.c and opens.c

--- srcs/exec/ms_exec.c
+++ srcs/exec/ms_exec.c
@@ -6,7 +6,7 @@
 /*   By: johnrandom <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 14:23:43 by johnrandom        #+#    #+#             */
-/*   Updated: 2025/04/03 09:51:01 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/03 10:56:44 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	ms_exec(t_list *tokkens, t_ms_context *context, t_ms_data *data)
 	t_files	*files;
 
 	temp = tokkens;
-	files = all_open(temp);
 	if (file == NULL)
 		return (1);
 	while (temp != NULL)
@@ -28,7 +27,7 @@ int	ms_exec(t_list *tokkens, t_ms_context *context, t_ms_data *data)
 		{
 			pid = fork();
 			if (pid == 0)
-				cmd_exec(tokken->content, data->env_var, context);
+				cmd_exec(temp, data->env_var, context);
 		}
 		temp = temp->next;
 	}
