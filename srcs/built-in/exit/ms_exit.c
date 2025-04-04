bb9cb496e3ca9296cf850491e42b3382e9568544
Arthur Girard <artgirar@c1r7p12.42lehavre.fr>
2025-04-04 17:38:46 +0200

    exit return updated and new define for exit returns

--- srcs/built-in/exit/ms_exit.c
+++ srcs/built-in/exit/ms_exit.c
@@ -6,22 +6,44 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:44:02 by maillet           #+#    #+#             */
-/*   Updated: 2025/04/04 16:46:49 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/04 17:35:38 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.function.h"
 
+static int	ms_exit_returns(char **exit_args, int i, int j, t_ms_data *data)
+{
+	if (i > 1)
+	{
+		ft_printfd(2, LANG_PREFIX "exit:" COUNT_WRONG);
+		return (EXIT_FAILURE);
+	}
+	if (j >= 1 && ft_str_is_num(exit_args[1]) == 0)
+	{
+		ft_printfd(2, LANG_PREFIX "exit:" COUNT_WRONG);
+		return (EXIT_FAILURE);
+	}
+	if (j >= 1)
+	{
+		ft_printfd(2, LANG_PREFIX "exit: %s : " NUMERIC_ERROR, exit_args[1]);
+		ms_close(2, data);
+	}
+	ms_close((unsigned char)ft_atol(exit_args[1]), data);
+	return (EXIT_FAILURE);
+}
+
 int	ms_exit(char *exit_status, t_ms_data *data)
 {
 	char	**exit_args;
-	int	i;
-	int	j;
-	int	k;
+	int		i;
+	int		j;
+	int		k;
 
+	ft_putstr_fd("exit\n", 2);
 	if (exit_status == NULL || exit_status[0] == '\0')
-		ms_close(EXIT_SUCCESS, data);
-	exit_args = ft_split(exit_status, ' ');
+		ms_close(data->last_return, data);
+	exit_args = ft_split_lst(exit_status, ' ');
 	if (ft_tabstr_len(exit_args) == 1)
 		ms_close((unsigned char)data->last_return, data);
 	i = 0;
@@ -35,12 +57,5 @@ int	ms_exit(char *exit_status, t_ms_data *data)
 			i++;
 		k++;
 	}
-	if (i > 1)
-		return (EXIT_FAILURE);
-	if (j >= 1 && ft_str_is_num(exit_args[1]) == 0)
-		return (EXIT_FAILURE);
-	if (j >= 1)
-		ms_close(2, data);
-	ms_close((unsigned char)ft_atol(exit_args[1]), data);
-	return (EXIT_FAILURE);
+	return (ms_exit_returns(exit_args, i, j, data));
 }
