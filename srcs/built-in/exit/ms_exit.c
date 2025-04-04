aff45858f9888947f11fbce9153a450840598c15
LolelsayFR <emaillet@student.42lehavre.fr>
2025-04-04 09:34:04 +0200

    Temp //

--- srcs/built-in/exit/ms_exit.c
+++ srcs/built-in/exit/ms_exit.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:44:02 by maillet           #+#    #+#             */
-/*   Updated: 2025/04/03 15:41:54 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/04 08:54:49 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int	ms_exit(char *exit_status, t_ms_data *data)
 {
+	(void)data;
 	if (exit_status == NULL || exit_status[0] == '\0')
 		(ft_alist_free(), exit(EXIT_SUCCESS));
-	else if ((unsigned char)ft_atoi(exit_status) == 0 && (exit_status[0] != '0'))
-	{
-		ft_printfd(2, LANG_EXIT_ERROR, ms_prefix(data), exit_status);	
-		return(EXIT_FAILURE);
-	}
+	// else if ((unsigned char)ft_atoi(exit_status) == 0 && (exit_status[0] != '0'))
+	// {
+	// 	ft_printfd(2, LANG_EXIT_ERROR, ms_prefix(data), exit_status);	
+	// 	return(EXIT_FAILURE);
+	// }
 	else
 		(ft_alist_free(),exit((unsigned char)ft_atoi(exit_status)));
 	return (EXIT_SUCCESS);
