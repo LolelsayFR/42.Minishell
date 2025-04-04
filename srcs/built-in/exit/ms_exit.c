474cd285d48cb9cb22730f91702f2132067207b6
Arthur Girard <artgirar@c1r7p12.42lehavre.fr>
2025-04-04 16:39:31 +0200

    exit advanced

--- srcs/built-in/exit/ms_exit.c
+++ srcs/built-in/exit/ms_exit.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:44:02 by maillet           #+#    #+#             */
-/*   Updated: 2025/04/04 15:10:40 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/04 16:12:48 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,28 @@
 int	ms_exit(char *exit_status, t_ms_data *data)
 {
 	char	**exit_args;
+	int	i;
+	int	j;
 
 	if (exit_status == NULL || exit_status[0] == '\0')
 		ms_close(EXIT_SUCCESS, data);
 	exit_args = ft_split(exit_status, ' ');
 	if (ft_tabstr_len(exit_args) == 1)
 		ms_close((unsigned char)data->last_return, data);
-	while (1)
+	i = 0;
+	j = 0;
+	while (*exit_args != NULL)
 	{
 		if (ft_str_is_num(*exit_args) == 1)
-			return (EXIT_FAILURE);
+			j++;
+		else if (ft_str_is_num(*exit_args) == 0)
+			i++;
 		exit_args++;
 	}
-	// else if ((unsigned char)ft_atoi(exit_status) == 0 && (exit_status[0] != '0'))
-	// {
-	// 	ft_printfd(2, LANG_EXIT_ERROR, ms_prefix(data), exit_status);	
-	// 	return(EXIT_FAILURE);
-	// }
-	ms_close((unsigned char)ft_atol(exit_status), data);
+	if (i > 1)
+		return (EXIT_FAILURE);
+	if (j >= 1)
+		ms_close(2, data);
+	ms_close((unsigned char)ft_atol(exit_args[1]), data);
 	return (EXIT_FAILURE);
 }
