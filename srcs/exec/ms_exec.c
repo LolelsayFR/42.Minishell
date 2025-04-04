822ab1ef114439838d479adebe36a218d02e0265
Arthur Girard <artgirar@c1r7p12.42lehavre.fr>
2025-04-04 12:36:50 +0200

    exec of cmd start

--- srcs/exec/ms_exec.c
+++ srcs/exec/ms_exec.c
@@ -6,14 +6,22 @@
 /*   By: johnrandom <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 14:23:43 by johnrandom        #+#    #+#             */
-/*   Updated: 2025/04/04 12:27:20 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/04 12:36:17 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.function.h"
 #include "exec.h"
 
-int	ms_exec(t_list *tokkens, t_ms_context *context, t_ms_data *data)
+void	cmd_exec(t_ms_tokken *tokken, char **env_var, t_files *files)
+{
+	(void)tokken;
+	(void)env_var;
+	(void)files;
+	exit(1);
+}
+
+int	ms_exec(t_list *tokkens, t_ms_data *data)
 {
 	t_list		*temp;
 	t_files		*files;
@@ -30,7 +38,7 @@ int	ms_exec(t_list *tokkens, t_ms_context *context, t_ms_data *data)
 		{
 			pids->pid = fork();
 			if (pids->pid == 0)
-				exit(1);//cmd_exec(tokken, data->env_var, context);
+				cmd_exec(tokken, data->env_var, files);
 			pids = pids->next;
 			pids = new_pids();
 		}
