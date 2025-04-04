ec2e14538308e928504a9310c980bc14da1242e2
Arthur Girard <artgirar@c1r7p12.42lehavre.fr>
2025-04-04 10:54:47 +0200

    opens.c modification

--- srcs/exec/opens.c
+++ srcs/exec/opens.c
@@ -6,7 +6,7 @@
 /*   By: artgirar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 10:00:40 by artgirar          #+#    #+#             */
-/*   Updated: 2025/04/04 10:42:40 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/04 10:52:25 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 void	file_error(t_files *files)
 {
+	files->fd = -1;
 	files->file_t = -1;
 }
 
@@ -30,7 +31,7 @@ void	outfile_open(t_ms_tokken *tokken, t_files *files)
 		return (file_error(files));
 	if (tokken->type == OUTF_R)
 		fd = open(tokken->content, O_CREAT | O_TRUNC | O_WRONLY, 0644);
-	if (tokken->type == OUTF_A)
+	else if (tokken->type == OUTF_A)
 	{
 		fd = open(tokken->content, O_CREAT | O_WRONLY, 0644);
 		while (read(fd, buff, 5) != 0)
