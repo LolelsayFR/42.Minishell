8fe75e42ec76247810d1ce20a31b2eb7b49be191
Arthur Girard <artgirar@c1r7p12.42lehavre.fr>
2025-04-04 09:50:05 +0200

    pull of headers

--- srcs/exec/exec.h
+++ srcs/exec/exec.h
@@ -6,7 +6,7 @@
 /*   By: artgirar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 09:06:32 by artgirar          #+#    #+#             */
-/*   Updated: 2025/04/04 09:31:27 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/04 09:46:09 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,10 @@ typedef struct s_files
 typedef struct s_pids
 {
 	pid_t			pid;
-	struct s_pids	next;
+	struct s_pids	*next;
 }	t_pids;
 
+t_files	*open_all(t_list *tokkens);
 void	files_clear(t_files *files);
 t_files	*new_files(void);
 
--- srcs/exec/ms_exec.c
+++ srcs/exec/ms_exec.c
@@ -6,7 +6,7 @@
 /*   By: johnrandom <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 14:23:43 by johnrandom        #+#    #+#             */
-/*   Updated: 2025/04/03 10:56:44 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/04 09:46:39 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,14 @@ int	ms_exec(t_list *tokkens, t_ms_context *context, t_ms_data *data)
 	t_files	*files;
 
 	temp = tokkens;
-	if (file == NULL)
-		return (1);
+	files = all_open(temp);
 	while (temp != NULL)
 	{
 		if (temp->content->type == CMD)
 		{
 			pid = fork();
 			if (pid == 0)
-				cmd_exec(temp, data->env_var, context);
+				cmd_exec(tokken->content, data->env_var, context);
 		}
 		temp = temp->next;
 	}
