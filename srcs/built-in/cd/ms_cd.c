7704f2f7bed67eb31b035df6421bc1ba4890a05d
Arthur Girard <artgirar@c1r1p4.42lehavre.fr>
2025-04-08 13:36:00 +0200

    cd updated with '~' on all situations

--- srcs/built-in/cd/ms_cd.c
+++ srcs/built-in/cd/ms_cd.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 12:20:45 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/07 18:49:56 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/08 13:33:53 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 int	ms_cd_exec(t_ms_data *data, char *pwd, char **split_path, char *path)
 {
+	char	*relat;
+
+	if (split_path[1][0] == '~')
+	{
+		relat = ft_strjoin(get_env(data, "HOME=") + 5, &split_path[1][1]);
+		printf("%s\n", relat);
+		path = relat;
+	}
 	if (chdir(path) == -1)
 	{
 		ft_printfd(2, LANG_PREFIX "cd: %s: " DIR_ERROR, split_path[1]);
+		if (split_path[1][0] == '~')
+			free(relat);
 		ft_free_strtab(split_path);
 		free(pwd);
 		return (1);
@@ -47,7 +57,7 @@ int	ms_cd(t_ms_data *data, char *path)
 		return (free(pwd), ft_printfd(2, LANG_PREFIX "cd: " DIR_COUNT), 1);
 	else if (split_path[1][0] == '/')
 		path = split_path[1];
-	else
+	else if (split_path[1][0] != '~')
 	{
 		ft_strcat(&pwd, "/");
 		ft_strcat(&pwd, split_path[1]);
