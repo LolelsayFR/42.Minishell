258c92667f09f66b6986c412ce3217debfb0f6c3
Arthur Girard <artgirar@c1r1p4.42lehavre.fr>
2025-04-08 15:06:45 +0200

    echo started with outfile redirection : need to talk about parsing to continue the code

--- srcs/built-in/echo/ms_echo.c
+++ srcs/built-in/echo/ms_echo.c
@@ -6,20 +6,63 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:44:02 by maillet           #+#    #+#             */
-/*   Updated: 2025/04/07 15:41:17 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/08 15:02:38 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.function.h"
 
-int	ms_echo(char *str, bool option)
+char	*write_all_args(t_ms_data *data)
 {
-	int	i;
+	char		*all_str;
+	t_list		*tokkens;
+	t_ms_tokken	*temp;
+	int			i;
 
+	tokkens = data->tokkens;
+	all_str = NULL;
 	i = 0;
-	while (str[i] != '\0')
-		write(1, &str[i++], 1);
-	if (option == 0)
-		write(1, "\n", 1);
-	return (0);
+	while (tokkens != NULL)
+	{
+		temp = tokkens->content;
+		if (temp->type == ARG)
+		{
+			if (i == 0)
+				i = 1;
+			else
+				ft_strcat(&all_str, " ");
+			ft_strcat(&all_str, temp->content);
+		}
+		tokkens = tokkens->next;
+	}
+	return (all_str);
+}
+
+int	ms_echo(t_ms_data *data, t_list *tokkens)
+{
+	int			i;
+	int			fd;
+	t_ms_tokken	*temp;
+	char		*save;
+
+	i = 5;
+	fd = -1;
+	save = NULL;
+	while (tokkens != NULL)
+	{
+		temp = tokkens->content;
+		if ((temp->type == OUTF_A || temp->type == OUTF_R))
+		{
+			if (fd != -1)
+				close(fd);
+			fd = open(temp->content, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+		}
+		tokkens = tokkens->next;
+	}
+	save = write_all_args(data);
+	if (fd == -1)
+		fd = 1;
+	while (save[i] != '\0')
+		write(fd, &save[i++], 1);
+	return (free(save), data->last_return = 0, 0);
 }
