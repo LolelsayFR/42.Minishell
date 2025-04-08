8ab0e3e2fa9e61b72e721aadee080c6ad9a40ef2
emaillet <emaillet@student.42lehavre.fr>
2025-04-08 16:50:19 +0200

    Better tokken debbuger view

--- srcs/utils/ms_tokken_printer.c
+++ srcs/utils/ms_tokken_printer.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 12:26:23 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/07 16:01:06 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/08 16:47:10 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 
 static void	print_tokken(t_ms_tokken *tokken)
 {
-	printf("\nTokken id = %d\nType : %d\nContent = %s\n\n", tokken->id,
-		tokken->type, tokken->content);
+	printf(GRN"\nTokken id = %d", tokken->id);
+	printf(YEL"\nType id : %d"RES, tokken->type);
+	if (tokken->type == INF)
+		printf("Type : INFILE");
+	if (tokken->type == H_D)
+		printf("Type : HEREDOC");
+	if (tokken->type == CMD)
+		printf("Type : COMMAND");
+	if (tokken->type == ARG)
+		printf("Type : ARGUMENT");
+	if (tokken->type == B_IN)
+		printf("Type : BUILT-IN");
+	if (tokken->type == OUTF_A)
+		printf("Type : OUTFILE APPEND");
+	if (tokken->type == OUTF_R)
+		printf("Type : OURFILE REPLACE");
+	printf(YEL"\nContent = %s\n"RES, tokken->content);
 }
 
 void	print_all_tokken(t_list *lst_head)
