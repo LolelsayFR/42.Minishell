740e019eb272e9c0c40386bd62f63daf699ce94b
emaillet <emaillet@student.42lehavre.fr>
2025-04-09 13:01:19 +0200

    Add Env struct

--- headers/minishell.define.h
+++ headers/minishell.define.h
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 10:47:04 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/08 15:50:06 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/09 12:58:45 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,6 @@
 
 # include "../submodules/42.libft/libft.h"
 # include <stdbool.h>
-
-/* ************************************************************************** */
-/*  Return value                                                              */
-/* ************************************************************************** */
-# define EXIT_SUCCESS 0
-# define EXIT_ERROR 1
 
 /* ************************************************************************** */
 /*  Tokken type                                                               */
@@ -33,10 +27,6 @@
 # define OUTF_A 4
 # define OUTF_R 5
 # define B_IN	6
-
-/* ************************************************************************** */
-/*  Typedef of types                                                          */
-/* ************************************************************************** */
 
 /* ************************************************************************** */
 /*  Typedef of struct                                                         */
@@ -59,6 +49,14 @@ typedef struct s_ms_context
 	int	nb_tkn;
 }	t_ms_context;
 
+//Env struct
+typedef struct s_env_list
+{
+	char				*var_name;
+	char				*var_cont;
+	struct s_env_list	*next;
+}	t_env_list;
+
 //All data stored in s_ms_data.
 typedef struct s_ms_data
 {
@@ -71,7 +69,9 @@ typedef struct s_ms_data
 	int				last_return;
 	t_ms_context	*context;
 	t_list			*tokkens;
+	t_env_list		*env_list;
 }	t_ms_data;
+
 
 //Parsing utils struct
 typedef struct s_pars_args
