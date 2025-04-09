54dc1c815c361b0a43f60583ec40e1498ca9d034
Arthur Girard <artgirar@c1r1p1.42lehavre.fr>
2025-04-09 09:09:36 +0200

    echo temporary

--- srcs/parsing/ms_prompt_handler.c
+++ srcs/parsing/ms_prompt_handler.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 09:30:33 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/08 17:00:42 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/09 09:08:31 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,7 @@ int	prompt_handler(t_ms_data *data)
 	else if (!ft_strncmp(data->prompt, "env", 3))
 		ms_env(data);
 	else if (!ft_strncmp(data->prompt, "echo", 4))
-		ms_echo(data, data->tokkens);
+		ms_echo(data, data->prompt, 1);
 	else if (!ft_strncmp(data->prompt, "cd", 2))
 		ms_cd(data, data->prompt);
 	print_all_tokken(data->tokkens);
