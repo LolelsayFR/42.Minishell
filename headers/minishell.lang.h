4f6a9047bda02f05bb027bd4e6d7db4a0d0ada0d
emaillet <emaillet@student.42lehavre.fr>
2025-04-10 18:53:51 +0200

    Some builtin clean

--- headers/minishell.lang.h
+++ headers/minishell.lang.h
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 10:47:01 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/10 18:29:01 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/10 18:51:35 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 /* ************************************************************************** */
 /*  Error and warn messages                                                   */
 /* ************************************************************************** */
-# define LANG_PARS_ERROR "%sParsing error : %s\n"
+# define LANG_PARS_ERROR "%sPrompt error : %s\n"
 # define LANG_MALLOC_ERROR "%sMalloc error : %s\n"
 # define EXIT_COUNT_WRONG "%sexit: too many arguments\n"
 # define EXIT_NUM_ERROR "%sexit: %s : numeric argument required\n"
-# define DIR_ERROR "No such file or directory\n"
-# define DIR_COUNT "too many arguments\n"
+# define DIR_ERROR "%scd: %s: No such file or directory\n"
+# define DIR_COUNT "%scd: too many arguments\n"
 
 /* ************************************************************************** */
 /*  Other language constant                                                   */
---
+++ srcs/built-in/ms_cd.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 12:20:45 by emaillet          #+#    #+#             */
+/*   Updated: 2025/04/10 18:37:30 by emaillet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.function.h"
+
+int	ms_cd_exec(t_ms_data *data, char *pwd, char **split_path, char *path)
+{
+	if (split_path[1][0] == '~')
+		path = ft_strjoin(get_env(data, "HOME=") + 5, &split_path[1][1]);
+	if (chdir(path) == -1)
+	{
+		ft_printfd(2, DIR_ERROR, ms_prefix(data), split_path[1]);
+		if (split_path[1][0] == '~')
+			free(path);
+		data->last_return = 1;
+		ft_free_strtab(split_path);
+		free(pwd);
+		return (1);
+	}
+	data->last_return = 0;
+	if (split_path[1][0] == '~')
+		free(path);
+	ft_free_strtab(split_path);
+	free(pwd);
+	return (0);
+}
+
+int	ms_cd(t_ms_data *data, char *path)
+{
+	char	*pwd;
+	char	*home;
+	char	**split_path;
+
+	pwd = getcwd(NULL, 0);
+	split_path = ft_split(path, ' ');
+	data->last_return = 1;
+	if (ft_tabstr_len(split_path) == 1)
+	{
+		home = get_env(data, "HOME=");
+		if (home == NULL)
+			return (free(pwd), ft_free_strtab(split_path), 1);
+		path = home + 5;
+	}
+	else if (ft_tabstr_len(split_path) > 2)
+		return (free(pwd), ft_printfd(2, DIR_COUNT, ms_prefix(data)), 1);
+	else if (split_path[1][0] == '/')
+		path = split_path[1];
+	else if (split_path[1][0] != '~')
+	{
+		ft_strcat(&pwd, "/");
+		ft_strcat(&pwd, split_path[1]);
+		path = pwd;
+	}
+	return (ms_cd_exec(data, pwd, split_path, path));
+}
