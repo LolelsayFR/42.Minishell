f2759cc641784ef65d6901ec770a8d97b89b89c5
emaillet <emaillet@student.42lehavre.fr>
2025-04-10 14:58:22 +0200

    New utils

---
+++ srcs/utils/ft_str_is_space.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_space.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/10 14:00:48 by emaillet          #+#    #+#             */
+/*   Updated: 2025/04/10 14:10:08 by emaillet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.function.h"
+
+bool	ft_str_is_space(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isspace(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
