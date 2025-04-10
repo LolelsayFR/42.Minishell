19efd5ca3d7a252ee4b25f2b3af358595bd0e85d
emaillet <emaillet@student.42lehavre.fr>
2025-04-10 19:19:45 +0200

    Sigint reactivate and fixed, some norm are now OK

--- srcs/signals.c
+++ srcs/signals.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sig_init.c                                         :+:      :+:    :+:   */
+/*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 09:14:40 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/07 15:41:17 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/10 19:17:48 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ static void	ms_sig_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		ft_printfd(1, "\n%s", ms_get_data()->prefix);
 		rl_replace_line("", 0);
 		rl_on_new_line();
+		printf("\n");
+		rl_redisplay();
 	}
 }
 
@@ -26,7 +27,7 @@ bool	ms_sig_init(t_ms_data *data)
 {
 	dumb(0, data);
 	signal(SIGQUIT, SIG_IGN);
-	//signal(SIGINT, ms_sig_handler);
+	signal(SIGINT, ms_sig_handler);
 	signal(SIGSEGV, ms_sig_handler);
 	return (true);
 }
