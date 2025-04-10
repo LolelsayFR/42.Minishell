349084198df8bd84b41489b353529b03487b669a
emaillet <emaillet@student.42lehavre.fr>
2025-04-10 18:56:00 +0200

    \super rgb like jeb_

--- srcs/prefix/prefix.c
+++ srcs/prefix/prefix.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 01:32:43 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/07 15:41:03 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/10 18:54:28 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ char	*ms_prefix(t_ms_data *data)
 {
 	if (data->prompt == NULL)
 		return (data->prefix);
-	if (data->easter_rgb == false && !strncmp(data->prompt, "rgb\0", 4))
+	if (data->easter_rgb == false 
+		&& !strncmp(data->prompt, "\\super rgb like jeb_", 20))
 		data->easter_rgb = true;
-	else if (data->easter_rgb == true && !strncmp(data->prompt, "rgb\0", 4))
+	else if (data->easter_rgb == true 
+		&& !strncmp(data->prompt, "\\super rgb like jeb_", 20))
 		data->easter_rgb = false;
 	if (data->easter_rgb == true)
 		return (ms_prefix_rgb_format(data->prompt, data));
