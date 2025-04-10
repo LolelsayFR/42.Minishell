8e2e39e0982e90bf9aa1053885e531dfe2b26d8f
emaillet <emaillet@student.42lehavre.fr>
2025-04-10 18:34:16 +0200

    Some builtin repath

--- srcs/built-in/ms_env.c
+++ srcs/built-in/ms_env.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:44:02 by maillet           #+#    #+#             */
-/*   Updated: 2025/04/08 15:13:37 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/10 18:32:52 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ms_env(t_ms_data *data)
 	i = 0;
 	while (data->env_var[i] != NULL)
 	{
-		ft_printf("%s\n", data->env_var[i]);
+		printf("%s\n", data->env_var[i]);
 		i++;
 	}
 	data->last_return = 0;
