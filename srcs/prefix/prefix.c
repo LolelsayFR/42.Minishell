7d7235af3340395c014bf14cc238092252c28477
emaillet <emaillet@student.42lehavre.fr>
2025-04-15 15:42:39 +0200

    Export WIP 2

--- srcs/prefix/prefix.c
+++ srcs/prefix/prefix.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 01:32:43 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/15 13:43:11 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/15 15:45:27 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ms_prefix(t_ms_data *data)
 	if (data->prompt == NULL)
 		return (data->prefix);
 	if (data->easter_rgb == false
-		&& !strncmp(data->prompt, "\"rgb like jeb_\"", 20))
+		&& !strncmp(data->prompt, "test super rgb like jeb_", 20))
 		data->easter_rgb = true;
 	else if (data->easter_rgb == true
-		&& !strncmp(data->prompt, "\"rgb like jeb_\"", 20))
+		&& !strncmp(data->prompt, "test super rgb like jeb_", 20))
 		data->easter_rgb = false;
 	if (data->easter_rgb == true)
 		return (ms_prefix_rgb_format(data->prompt, data));
