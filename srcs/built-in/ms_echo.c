68a73d59facca671189861472a01ac73ae24999e
Arthur Girard <artgirar@c1r7p10.42lehavre.fr>
2025-04-16 14:10:47 +0200

    echo message of write does not function is check

--- srcs/built-in/ms_echo.c
+++ srcs/built-in/ms_echo.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:44:02 by maillet           #+#    #+#             */
-/*   Updated: 2025/04/16 09:23:21 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/16 14:08:02 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 
 int	ms_echo(char *str, int arg)
 {
-	int			i;
+	int	i;
+	int	k;
 
 	i = 0;
+	k = 0;
 	if (str == NULL)
 	{
 		if (arg == 0)
-			write(1, "\n", 1);
-		return (0);
+			k = write(1, "\n", 1);
 	}
-	while (str[i] != '\0')
-		write(1, &str[i++], 1);
-	if (arg == 0)
-		write(1, "\n", 1);
+	else
+	{
+		while (str[i] != '\0' && k != -1)
+			k = write(1, &str[i++], 1);
+		if (arg == 0 && k != -1)
+			k = write(1, "\n", 1);
+	}
+	if (k == -1)
+		ft_printfd(2, LANG_PREFIX "write error: no space left to write\n");
 	return (0);
 }
