3fbff0a451224d7effc1d46e5e3c755bef7a8b8c
emaillet <emaillet@student.42lehavre.fr>
2025-04-16 15:30:13 +0200

    echo error prefix update

--- srcs/built-in/ms_echo.c
+++ srcs/built-in/ms_echo.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:44:02 by maillet           #+#    #+#             */
-/*   Updated: 2025/04/16 17:17:24 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/16 17:22:05 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,6 @@ int	ms_echo(char *str, int arg)
 			k = write(1, "\n", 1);
 	}
 	if (k == -1)
-<<<<<<< HEAD
 		ft_printfd(2, ECHO_NO_SPACE, ms_prefix(ms_get_data()));
-=======
-		ft_printfd(2, ECHO_NO_SPACE, ms_prefix());
->>>>>>> 3ca9ddf (Echo message fix)
 	return (0);
 }
