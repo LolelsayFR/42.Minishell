045ee78905537b727d26776a8c7cf8f548465958
Arthur Girard <artgirar@c1r7p10.42lehavre.fr>
2025-04-16 15:49:26 +0200

    little changes:

--- srcs/utils/get_env.c
+++ srcs/utils/get_env.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   go_to_home.c                                       :+:      :+:    :+:   */
+/*   get_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: artgirar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 18:03:54 by artgirar          #+#    #+#             */
-/*   Updated: 2025/04/07 18:27:50 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/16 15:48:04 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*get_env(t_ms_data *data, char *var)
 	char	**env;
 
 	env = data->env_var;
+	if (env == NULL)
+		return (NULL);
 	while (*env != NULL && ft_strncmp(*env, var, ft_strlen(var)) != 0)
 		env++;
 	return (*env);
