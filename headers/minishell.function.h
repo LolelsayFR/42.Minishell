8cc45967c01ff77a164b01d3d9e0d1024e67f805
Arthur Girard <artgirar@c1r7p10.42lehavre.fr>
2025-04-17 12:24:11 +0200

    export changes

--- headers/minishell.function.h
+++ headers/minishell.function.h
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 10:46:42 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/17 18:37:35 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/17 18:40:23 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,6 +120,7 @@ int			ms_ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*str_unquote(char *str);
 void		env_export(char *name, char *content, t_env_lst **head);
 char		*pars_injector(char *str, char *temp, t_pars_args *arg);
+void		env_export(char *name, char *content, t_env_lst **head);
 
 #endif
 /* ************************************************************************** */
