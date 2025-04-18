1286be4da0db0cf451b2503096fe8328c45d40f5
emaillet <emaillet@student.42lehavre.fr>
2025-04-18 16:06:35 +0200

    Prefix update

--- srcs/prefix/prefix.c
+++ srcs/prefix/prefix.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 01:32:43 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/18 12:09:32 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/18 15:50:00 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 char	*ms_prefix(t_ms_data *data)
 {
+	const char *custom = get_env_var(data, "ms_prefix");
+
 	if (data->prompt == NULL)
 		return (data->prefix);
+	if (custom != NULL)
+		data->prefix = get_env_var(data, "ms_prefix") + 10;
+	else
+		data->prefix = LANG_PREFIX;
 	if (get_env_lst(data, "ms_rgb") != NULL)
 		data->easter_rgb = true;
 	else
 		data->easter_rgb = false;
 	if (data->easter_rgb == true)
-		return (ms_prefix_rgb_format(data->prompt, data));
+		return (ms_prefix_rgb_format(data->prefix, data));
 	return (data->prefix);
 }
