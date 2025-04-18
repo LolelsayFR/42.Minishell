ee111fe3a5d619b6f27946eed1cfd34b39fcb96b
emaillet <emaillet@student.42lehavre.fr>
2025-04-18 11:21:47 +0200

    Init some env var

--- headers/minishell.function.h
+++ headers/minishell.function.h
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 10:46:42 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/17 18:41:12 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/18 10:32:40 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,12 +120,7 @@ char		**args_split(int i, char *str);
 int			ms_ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*str_unquote(char *str);
 char		*pars_injector(char *str, char *temp, t_pars_args *arg);
-void		env_export(char *name, char *content, t_env_lst **head);
-char		*pars_injector(char *str, char *temp, t_pars_args *arg);
-void		env_export(char *name, char *content, t_env_lst **head);
-char		*pars_injector(char *str, char *temp, t_pars_args *arg);
-void		env_export(char *name, char *content, t_env_lst **head);
-char		*pars_injector(char *str, char *temp, t_pars_args *arg);
+int 		env_export(char *name, char *content, t_env_lst **head);
 
 #endif
 /* ************************************************************************** */
--- srcs/utils/env_export.c
+++ srcs/utils/env_export.c
@@ -3,20 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   env_export.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: artgirar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 12:20:01 by artgirar          #+#    #+#             */
-/*   Updated: 2025/04/17 18:40:44 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/18 10:47:05 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.function.h"
 
-void	env_export(char *name, char *content, t_env_lst **head)
+
+static t_env_lst	*new_env(char *name, char *content)
+{
+	t_env_lst	*new;
+
+	new = ft_calloc(1, sizeof(t_env_lst));
+	new->next = NULL;
+	new->var_name = name;
+	if (content == NULL)
+		new->var_cont = NULL;
+	else
+		new->var_cont = ft_strdup(content);
+	return (ms_get_data()->env_var = env_to_tab(new), new);
+}
+
+int	env_export(char *name, char *content, t_env_lst **head)
 {
 	t_env_lst	*new;
 	t_env_lst	*tmp;
 
+	ft_free_strtab(ms_get_data()->env_var);
+	if (ms_get_data()->env_lst == NULL)
+		return (ms_get_data()->env_lst = new_env(name, content), 1);
 	if (ft_strlen(content) == 0)
 		(free(content), content = NULL);
 	new = NULL;
@@ -27,14 +45,11 @@ void	env_export(char *name, char *content, t_env_lst **head)
 	{
 		new = ft_calloc(1, sizeof(t_env_lst));
 		tmp->next = new;
-		new->next = NULL;
 		new->var_name = name;
-		if (content == NULL)
-			new->var_cont = NULL;
-		else
+		if (content != NULL)
 			new->var_cont = ft_strdup(content);
 	}
 	else if (content != NULL)
 		(tmp->var_cont = ft_strdup(content));
-	free(content);
+	return (ms_get_data()->env_var = env_to_tab(*head), free(content), 0);
 }
