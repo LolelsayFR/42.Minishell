0217297554626436bbae84eb6eed6fd6da2ddbfd
emaillet <emaillet@student.42lehavre.fr>
2025-04-24 13:42:26 +0200

    ft_is_ll fixes

--- srcs/utils/ft_is_ll.c
+++ srcs/utils/ft_is_ll.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:00:48 by artgirar          #+#    #+#             */
-/*   Updated: 2025/04/10 13:03:31 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/24 13:39:42 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_is_ll(char *str)
 	if (ft_strlen(str) > ft_strlen("9223372036854775807"))
 		return (1);
 	if (ft_strncmp("9223372036854775808", str, ft_strlen(str)) == 0)
-		return (0);
+		return (1);
 	if (ft_atoll(str) <= 0)
 		return (1);
 	return (0);
