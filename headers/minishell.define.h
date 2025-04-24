f0ad801b1e8180b494b115cb1e52abdeaeee3b6a
emaillet <emaillet@student.42lehavre.fr>
2025-04-24 12:26:41 +0200

    Hide debug

--- headers/minishell.define.h
+++ headers/minishell.define.h
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 10:47:04 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/24 11:47:39 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/24 12:26:28 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 /* ************************************************************************** */
 
 //Global Debug mod
-# define MS_DEBUG true
+# define MS_DEBUG false
 //Tokken printer
 # define MS_DE_TOKPRINT true
 //Split_tab parsing printer
