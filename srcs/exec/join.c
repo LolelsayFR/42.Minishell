49a9978099dba461319e3cfead08fe2cec88540c
Arthur Girard <artgirar@c1r7p10.42lehavre.fr>
2025-04-29 11:25:55 +0200

    new join for expand argument/commands

--- srcs/exec/join.c
+++ srcs/exec/join.c
@@ -6,11 +6,55 @@
 /*   By: artgirar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 19:23:31 by artgirar          #+#    #+#             */
-/*   Updated: 2025/04/28 10:33:24 by artgirar         ###   ########.fr       */
+/*   Updated: 2025/04/29 11:24:31 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.function.h"
+
+static int	arg_in_id(t_list *tokkens, int id)
+{
+	int			i;
+	int			j;
+	char		**temp;
+	t_ms_tokken	*tokken;
+
+	i = 0;
+	while (tokkens != NULL)
+	{
+		tokken = tokkens->content;
+		if (tokken->id != id)
+			break ;
+		if (tokken->flag == ISEXPAND)
+		{
+			j = 0;
+			temp = ft_split(tokken->content, ' ');
+			while (temp[j++] != NULL)
+				i++;
+			ft_free_strtab(temp);
+		}
+		else
+			i++;
+		tokkens = tokkens->next;
+	}
+	return (i);
+}
+
+static void	add_tab_in_tab(char **cmd, char *content, int *i)
+{
+	int	j;
+	char	**temp;
+
+	j = 0;
+	temp = ft_split(content, ' ');
+	while (temp[j] != NULL)
+	{
+		cmd[*i] = ft_strdup(temp[j]);
+		(*i)++;
+		j++;
+	}
+	ft_free_strtab(temp);
+}
 
 char	**tokken_id_join(t_list *tokkens, int id)
 {
@@ -21,13 +65,16 @@ char	**tokken_id_join(t_list *tokkens, int id)
 
 	i = 0;
 	temp = first_in_id(tokkens, id);
-	cmd = ft_calloc((arg_nb(temp, id) + 1), sizeof(char *));
+	cmd = ft_calloc((arg_in_id(temp, id) + 1), sizeof(char *));
 	while (temp != NULL)
 	{
 		tokken = temp->content;
 		if (tokken->id != id)
 			break ;
-		if (tokken->type == CMD || tokken->type == ARG
+		if ((tokken->type == CMD || tokken->type == ARG
+			|| tokken->type == B_IN) && tokken->flag == ISEXPAND)
+			add_tab_in_tab(cmd, tokken->content, &i);
+		else if (tokken->type == CMD || tokken->type == ARG
 			|| tokken->type == B_IN)
 			cmd[i++] = ft_strdup(tokken->content);
 		temp = temp->next;
