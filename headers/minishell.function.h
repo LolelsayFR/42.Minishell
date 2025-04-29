d1ebc5f66720f6e909f4618bf6ef9b5d521c89de
Arthur Girard <artgirar@c1r7p10.42lehavre.fr>
2025-04-29 16:50:19 +0200

    norm on header

--- headers/minishell.function.h
+++ headers/minishell.function.h
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 10:46:42 by emaillet          #+#    #+#             */
-/*   Updated: 2025/04/29 16:18:24 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/04/29 16:49:49 by artgirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -178,7 +178,7 @@ char		*pars_injector(char *str, char *temp, t_pars_args *arg);
 int			env_export(char *name, char *content, t_env_lst **head);
 char		*ft_strjointab(char **tab);
 int			ms_strcmp_e(char *s1, const char *s2);
-int	        ms_unset_ex(t_ms_data *data, char *arg);
+int			ms_unset_ex(t_ms_data *data, char *arg);
 
 #endif
 /* ************************************************************************** */
